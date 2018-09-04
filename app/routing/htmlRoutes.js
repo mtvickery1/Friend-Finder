8b7cda875443064b4a91c3b7e500ae4cfea85ad9
mv04 <mtvickery1@gmail.com>
2018-09-04 11:39:23 -0500

    added catch all to htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,6 +9,10 @@ function htmlRoutes(app) {
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 };
 
 module.exports = htmlRoutes;
